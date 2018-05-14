4861c4342a1e3a38240d6e5cc6e78e6c65e06655
patel-km <karipatel95@hotmail.com>
2018-05-14 11:25:36 -0400

    js out of html and into .js file

---
+++ assets/javascript/app.js
@@ -0,0 +1,21 @@
+//Home screen
+            //click start to start the game
+            $("#start").on("click" game() );
+            
+            //switch to showing "game" div
+            function game () {
+
+        }
+        //game includes:
+            //timer ticking down for full quiz (maybe 120 sec) so it auto calls done function when timer runs out
+            //list of questions
+            //answer choices for each question
+            //selecting only one answer per question using radio button
+            //done button to click if user finishes before time's up
+            ///switch to showing "endScreen" div
+            
+
+        //when timer hits zero, done screen
+            //calculates and shows correct answers, incorrect answers, and unanswered.
+
+        //just reload page to start again, so no "try again" button. Can make this feature if time permits
